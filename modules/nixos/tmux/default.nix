a7e1c1d223fde9e8078c4feaf6509cdd3ce42c18
goodrod <david.lindskog.hedstrom@pm.me>
2024-06-22 16:10:19 +0200

    Fix home-manager NixOS module integration, add tmux module

--- modules/nixos/tmux/default.nix
+++ modules/nixos/tmux/default.nix
@@ -3,27 +3,27 @@ with lib;
 let
   # Shorter name to access final settings a 
   # user of hello.nix module HAS ACTUALLY SET.
-  # cfg is a typical convention. But notice how
-  # option makes much more sense.
-  option = config.module.placeholder;
+  # cfg is a typical convention.
+  cfg = config.module.tmux;
 in {
   imports = [
     # Paths to other modules.
     # Compose this module out of smaller ones.
   ];
 
-  options.module.placeholder = {
+  options.module.tmux = {
     # Option declarations.
     # Declare what settings a user of this module module can set.
     # Usually this includes a global "enable" option which defaults to false.
-    enable = mkEnableOption "placeholder";
+    enable = mkEnableOption "tmux";
   };
 
-  config = mkIf option.enable {
+  config = mkIf cfg.enable {
     # Option definitions.
     # Define what other settings, services and resources should be active.
     # Usually these depend on whether a user of this module chose to "enable" it
     # using the "option" above. 
     # Options for modules imported in "imports" can be set here.
+    programs.tmux.enable = true;
   };
 }
