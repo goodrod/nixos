8ea24e56eec61c679c0144c6e0ed5198b129e6ba
David Lindskog Hedström <david.lindskog-hedstrom@helisoft.se>
2024-07-07 22:43:21 +0200

    Sync work host config with latest private host changes

--- hosts/work/home.nix
+++ hosts/work/home.nix
@@ -5,7 +5,10 @@
   module.awesome.enable = false;
   module.hyprland = {
     enable = true;
-    monitors.left = { enable = true; };
+    monitors.left = { 
+      name = "eDP-1";
+      enable = true;
+    };
     monitors.middle = {
       enable = true;
       name = "DP-5";
@@ -19,9 +22,9 @@
   };
   module.waybar.enable = true;
   module.alacritty.enable = true;
-  module.vscode.enable = true;
   module.tmux.enable = true;
   module.default-home-dirs.enable = true;
+  module.dunst.enable = true;
   module.wofi.enable = true;
   home.packages = [ pkgs.ripgrep ];
 
